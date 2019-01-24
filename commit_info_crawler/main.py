2d541f46d89fa1796f4e63a5f52a703b7ec3f6fb
HwangDongJun <enter031@naver.com>
2019-01-24 15:31:59 +0900

    Sort and output in order of the number of commit times

--- commit_info_crawler/main.py
+++ commit_info_crawler/main.py
@@ -1,4 +1,5 @@
 import sys
+import operator
 from pprint import pprint
 
 from get_certification import git_certification
@@ -12,18 +13,34 @@ def search_commit(argv):
 	with open(argv[1], 'r') as fr:
 		lines = fr.readlines()
 		user_cert = input("Type in the following format(userID:userPWD): ")
+		ranking_user = dict()
 		for line in lines:
 			user_name = line.strip()
 			Ucert = git_certification(user_cert)
 			cert_info = Ucert.get_info2base64()
 			
-			header = {'Authorization' : ('Basic ' + cert_info)}
+			header = {'Authorization' : ('Basic ' + cert_info)} # make haeder
 			commit_info = repo_commit_info(header, user_name)
 			repo_names = commit_info.get_repo_info()
 			for repo in repo_names:
 				commit_crawler = graphql_api_crawler(repo, header, user_name)
 				commit_data = commit_crawler.run_query()
 				pprint(commit_data)
+				
+		#Choose the case with the highest number of commit attempts.
+				if commit_data['data']['repository']['defaultBranchRef'] == None:
+					continue
+				total_count = commit_data['data']['repository']['defaultBranchRef']['target']['history']['totalCount']
+				if user_name not in ranking_user:
+					ranking_user[user_name] = total_count
+				elif ranking_user[user_name] < total_count:
+					ranking_user[user_name] = total_count
+		
+		sorted(ranking_user.items(), key=operator.itemgetter(1), reverse=True)
+		count = 1
+		for rank in ranking_user:
+			print("Rank {} -> Name: {} | Count: {}".format(str(count), rank, ranking_user[rank]))
+			count += 1
 
 if __name__ == '__main__':
 	sys.exit(search_commit(sys.argv))
